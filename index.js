d241c6aaeea1dbf93f68d47d02a3a8c059f9082a
sharon <sharonoptsout@gmail.com>
2018-04-25 23:28:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function breakOut(array, changeValue, stopValue) {
+  for(let i = 0; i<array.length; i++){
+    if(i===stopValue) {
+      break;
+    }
+    array[i]=changeValue;
+  }
+  return(array);
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for(let i = 0; i<array.length; i++){
+    if (array[i] === skipValue) {
+      continue
+    }
+    array[i] = changeValue;
+  }
+  return(array);
+}
