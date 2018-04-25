c056e902484be47b4b688c95cae7f8ead894ccc0
sharon <sharonoptsout@gmail.com>
2018-04-25 23:36:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function breakOut(array, changeValue, stopValue) {
   for(let i = 0; i<array.length; i++){
-    if(i===stopValue) {
+    if(array[i]===stopValue) {
       return(array);
     }
     array[i]=changeValue;
