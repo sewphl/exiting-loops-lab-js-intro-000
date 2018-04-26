dbcd002292bef7d03b292a5a0354f858fd379886
sharon <sharonoptsout@gmail.com>
2018-04-26 00:07:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,10 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 function findBy(array, findFn) {
-
+  for(let i = 0; i<array.length; i++){
+  if(findFn(array[i])){
+    return(array[i]);
+  }
+}
+  return(null);
 }
